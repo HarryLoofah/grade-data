ed1080fbc9bf29a9babed82297ec71bcbb293bd9
HarryLoofah <harryloofah@gmail.com>
2015-07-14 10:38:21 -0600

    clean up code

--- grade_data/main.py
+++ grade_data/main.py
@@ -10,43 +10,36 @@ on individual student progress, but plots overall classroom trends.
 The program reads a previously downloaded gradebook spreadsheet then converts
 'number +/-' scores and standards based scores to numeric values, and handles
 'missing' or 'excused' values.  Afterwards, scores will be processed so that
-growth data can be observed via plotting.  Also, program assumes input is .csv
-since this is the only file download format available.
+growth data can be observed via plotting.  Program assumes input is .csv.
 """
 
 __version__ = "1.0"
 
-# imports
 import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Get / store .csv file.
 IMPORT_CSV = raw_input("Enter the name of the .csv file to import: ")
-# Get / store students' language level.
 LANG_LEVEL = raw_input("Enter language level (single number) for class: ")
-# Get teacher name or initials.
 TEACHER_NAME = raw_input("Enter teacher's name or initials: ")
-# Get current date.
 TIME = datetime.datetime.now()
-# Format date.
-NOW = TIME.strftime("%Y-%m-%d %H:%M")
+NOW = TIME.strftime("%Y-%m-%d %I:%M")
 
 def clean_data():
     '''
-    Function cleans up data by stripping header lines, student names, and
-    converting grades to numeric values.  Returns a data frame ready to plot.
+    Cleans data by stripping header lines, student names, and converting various
+    types of grades to numeric values.  Returns a data frame ready to plot.
     '''
     # Import .csv and eliminate header rows.
-    df_read = pd.read_csv(IMPORT_CSV, skiprows=2, header=None)
-    # Remove column 0 which contains student names
-    df_drop_names = df_read.drop(0, 1)
-    # Drop columns if all values are NaN (i.e. no grades were entered)
-    df_drop_col = df_drop_names.dropna(axis=1, how='all')
+    df = pd.read_csv(IMPORT_CSV, skiprows=2, header=None)
+    # Remove column 0 (which contains student names).
+    df = df.drop(0, 1)
+    # Drop columns if all values are NaN (i.e. no grades were entered).
+    df = df.dropna(axis=1, how='all')
     # Replace "excused" and "missing" values with NaN.
-    df_no_excuse = df_drop_col.replace(("e", "m", "E", "M"), "NaN", regex=True)
-    # Convert standards based grades (u, pp, p, a) to numerical grade based on
-    # classroom language level.
+    df = df.replace(("e", "m", "E", "M"), "NaN", regex=True)
+
+    # Change standards based grades to numerical grade based on language level.
     to_replace = ('u', 'pp', 'p', 'a')
     value = (
         (int(LANG_LEVEL) - 1),
@@ -54,40 +47,36 @@ def clean_data():
         (int(LANG_LEVEL)),
         (int(LANG_LEVEL) + .5),
         )
-    df_standard_free = df_no_excuse.replace(to_replace, value, regex=True)
-    # Convert values from 'number +/-' to numerical (based on lang. level).
-    # Use .3 for '+'s and .7 for '-'s (i.e. 2- == 1.7)
+    df = df.replace(to_replace, value, regex=True)
+
+    # Convert values from 'number +/-' to numerical.
     pm_replace = ('1+', '2-', '2+', '3-', '3+', '4-', '4+', '5-', '5+', '6-')
     pm_values = (1.3, 1.7, 2.3, 2.7, 3.3, 3.7, 4.3, 4.7, 5.3, 5.7)
-    df_converted = df_standard_free.replace(pm_replace, pm_values, regex=True)
-    # Convert all dataframe values to float = clean dataframe
-    df_clean = df_converted.astype(float)
-    return df_clean
+    df = df.replace(pm_replace, pm_values, regex=True)
 
-def calculate_column_mean(df_clean):
-    '''
-    Calculate mean of each column (mean of each assignment's scores) and return.
-    '''
-    df_mean = df_clean.mean(axis=0)
+    df = df.astype(float)
+
+    return df
+
+def calculate_column_mean(df):
+    df_mean = df.mean(axis=0)
     return df_mean
 
 def plot_df(df_mean):
-    '''
-    Plot the mean.
-    '''
-    # Define font for title and labels.
+
     font = {'family' : 'sans',
             'color'  : 'darkred',
             'weight' : 'normal',
             'size'   : 14,}
-    # Create plot with title and labels.
+
     df_mean.plot(kind='bar')
     plt.title('Teacher: {} -- Created: {}'.format(TEACHER_NAME, NOW))
     plt.xlabel('Assignments', fontdict=font)
     plt.ylabel('Scores', fontdict=font)
+
     plt.show()
 
-if __name__ == "__main__":  # This seems wrong, but couldn't make it work w/o.
+if __name__ == "__main__":
     clean_data()
     calculate_column_mean(clean_data())
     plot_df(calculate_column_mean(clean_data()))
